419719e26b914cf75dc6caaee6e33ed3ef72084d
Juana Mignone <juanamignone135@gmail.com>
2024-03-17 19:37:49 -0300

    desafio 2

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+const fs = require("fs")
+
+
 class ProductManager{
     //Atributos
     products;
-    static id = 0;
+    path;
 
     //Constructor
     constructor(){
-        this.products = []
+        this.products = this.#leerArchivo()
+        this.path = './archivos/productos.txt'
     }
+
     //Métodos
+    #idGenerator(){
+        let id = 1
+        if(this.products.length !== 0)
+        id= this.products[this.products.length-1].id+ 1
+        return id
+    }
+
+
+    #leerArchivo(){
+        try {
+            if(fs.existsSync(this.path))
+            return JSON.parse(fs.readFileSync(this.path, 'utf-8'))
+            return []
+        } catch (error) {
+            console.log(error.message) 
+        }
+    }
+
+    #guardarDatos(){
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.products))
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+
     addProduct(title,description,price,thumbnail,code,stock){
         if(!title || !description || !price || !thumbnail || !code || !stock){
         return 'Faltan campos'
@@ -20,7 +52,7 @@ class ProductManager{
         
 
         ProductManager.id = ProductManager.id + 1
-        const id = ProductManager.id
+        const id = this.#idGenerator()
         
 
         const nuevoProducto = {
@@ -33,6 +65,7 @@ class ProductManager{
             stock: stock
         }
         this.products.push(nuevoProducto)
+        this.#guardarDatos()
         return "Producto agregado"
     }
     getProducts(){
@@ -45,6 +78,26 @@ class ProductManager{
         } else{
             return "No se encontró el producto :("
         }
+    }
+    updateProduct(id, propiedad){
+        const i = this.products.findIndex(p=> p.id === id)
+        if(i !== -1){
+            const {id, ...rest} = propiedad
+            this.products[i] = {...this.products[i], ...rest}
+            this.#guardarDatos
+            return 'El producto fue actualizado correctamente'
+        }
+    }
+
+
+    deleteProduct(id){
+        const i = this.products.findIndex(p=> p.id === id)
+        if(i !== -1){
+            this.products = this.products.filter(p=>p.id !== id)
+            this.#guardarDatos()
+            return (`Producto eliminado correctamente!`)
+        }
+        return (`No existe el producto ${id}`)
     }
 }
 
